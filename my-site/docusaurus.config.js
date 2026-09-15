622eeca21ecc3cce52eaced838225810005799a7
Aku987 <akanksha.sarpal@gmail.com>
2026-09-15 16:31:06 +0530

    new

--- my-site/docusaurus.config.js
+++ my-site/docusaurus.config.js
@@ -82,9 +82,9 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'docs',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
