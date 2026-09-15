c3eb0e55096208289c03f570556fbb6e76d0d3ee
Aku987 <akanksha.sarpal@gmail.com>
2026-09-15 12:36:20 +0530

    updating

--- my-site/docusaurus.config.js
+++ my-site/docusaurus.config.js
@@ -1,17 +1,8 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import {themes as prismThemes} from 'prism-react-renderer';
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'AS Docs',
+  tagline: 'Documentation for AS',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
